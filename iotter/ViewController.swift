2bcba8e76dbc303ec064334daade802a6636d0f1
Joe Doherty <joe@pjuu.com>
2015-08-04 18:15:10 +0100

    Initial iOtter commit.
    The app is pretty much ready. It needs images, polishing and more
    testing.

---
+++ iotter/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  iotter
+//
+//  Created by Joe on 02/08/2015.
+//  Copyright (c) 2015 Pjuu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIWebViewDelegate {
+
+    @IBOutlet var webView: UIWebView!
+
+    let hostName = "pjuu.com"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Clear the cache so we don't get stuck on the maintenance
+        // page or anything else. Only when the view loads though.
+        NSURLCache.sharedURLCache().removeAllCachedResponses()
+        
+        let url = NSURL(string: "https://\(hostName)")
+        
+        webView.delegate = self
+        let request = NSURLRequest(URL: url!)
+        
+        webView.loadRequest(request)
+    }
+
+    func webView(WebViewNews: UIWebView, shouldStartLoadWithRequest request: NSURLRequest,      navigationType: UIWebViewNavigationType) -> Bool {
+        
+        let requestURL = request.URL!
+
+        if requestURL.host == hostName {
+            return true
+        } else {
+            UIApplication.sharedApplication().openURL(requestURL)
+            return false
+        }
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+    }
+
+    func webViewDidFinishLoad(webView: UIWebView) {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+    }
+    
+}
